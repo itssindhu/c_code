fbea7d088f3a1f4034f56b5539838a8fb64da130
itssindhu <sindhujachinraj@gmail.com>
2018-07-30 20:08:59 +0530

    To print the given N numbers

---
+++ N_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+ int index=1,number;
+ printf("Enter the Number:");
+ scanf("%d",&number);
+for(index=1;index<=number;index++)
+ {
+  printf("%d\n",index);
+}
+return 0;
+}
